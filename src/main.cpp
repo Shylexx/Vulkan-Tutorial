85314e0733cf44a8e7a76d275fdc287fddec1fd0
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-07-11 21:22:33 +0100

    Create Vk Instance

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ private:
     }
     void initVulkan()
     {
+        createInstance();
     }
 
     void mainLoop()
@@ -48,6 +49,43 @@ private:
         glfwTerminate();
     }
 
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // GLFW Get Extensions
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // Specify Validation Layers
+        createInfo.enabledLayerCount = 0;
+
+        // Create the instance
+
+        if (vkCreateInstance(&createInfo, nullptr, &m_Instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to Create Vulkan Instance");
+        }
+        std::cout << "Vulkan Instance Created Successfully" << std::endl;
+    }
+
+private:
+    VkInstance m_Instance;
     GLFWwindow *m_Window;
 };
 
